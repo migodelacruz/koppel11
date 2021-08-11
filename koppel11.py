b246b27d998edfe430733d3917294ae016053583
Ubuntu <ubuntu@ip-172-31-35-104.ap-southeast-2.compute.internal>
2021-08-11 05:23:15 +0000

    Add Repetitions parameter as input argument

--- koppel11.py
+++ koppel11.py
@@ -9,7 +9,7 @@ featureLength = 20000
 # Score threshold (needed for open set)
 threshold = 0
 # number of k repetitions
-repetitions = 500
+# repetitions = 500
 # minimum size of document
 # (increases precision, but deteriorates recall,
 # if there are many small documents)
@@ -173,10 +173,13 @@ def main():
         description="Tira submission for PPM approach (koppel11)")
     parser.add_argument("-i", action="store", help="path to corpus directory")
     parser.add_argument("-o", action="store", help="path to output directory")
+    parser.add_argument("-r", action="store", help="number of repetitions")
     args = vars(parser.parse_args())
 
     corpusdir = args["i"]
     outputdir = args["o"]
+    repetitions = int(args["r"])
+
     if corpusdir == None or outputdir == None:
         parser.print_help()
         return
